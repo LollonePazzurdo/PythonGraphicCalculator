da4de86f014e61c5177e61d0481471c12877f53d
LollonePazzurdo <lorenzogiovannini774@gmail.com>
2026-07-17 12:40:59 +0200

    refactor(test_main): Improve multithreading test implementation and update equation examples

--- test_main.py
+++ test_main.py
@@ -16,7 +16,8 @@ def draw_multithreading_test(g: Graph, eqs:list[str]):
     t0 = time.perf_counter()
     threads = []
     for eq in eqs:
-        t = threading.Thread(target=lambda g, eq: g.draw(eq, color=colors.r_color()), args=(g, eq))
+        # race condition is definetly a possibility here.
+        t = threading.Thread(target=g.draw, args=(eq,), kwargs={'color': colors.r_color()})
         t.start()
         threads.append(t)
     for t in threads:
@@ -26,14 +27,12 @@ def draw_multithreading_test(g: Graph, eqs:list[str]):
 
 def draw_multiprocessing_test(g: Graph, eqs:list[str]):
     t0 = time.perf_counter()
-    plots = []
-    for eq in eqs:
-        plots.append(Plot(equation=eq, color=colors.r_color(), size=g.size, zoom=g.zoom, domain="True", center=g.center))
+    plots = g.get_plots(equations=eqs, colors=[colors.r_color() for _ in range(len(eqs))])
     g.draw_plots(plots)
     return time.perf_counter()-t0
 
 
-scale = 1
+scale = 2
 g = Graph(
     view=2, 
     size=400//scale,
@@ -45,24 +44,26 @@ g = Graph(
 
 
 eqs = [
-    "y=x",
-    "y=-x",
-    "y=x^2",
-    "y=-x^2",
+    "y=cos(x)",
+    "y=-cos(x)",
+    "x=cos(y)",
+    "x=-cos(y)",
 ]
 
-print(f"Sequential time: {draw_sequential_test(g, eqs)}")
-cv2.imshow("Graph", g.img)
-cv2.waitKey(0)
-g.reset()
+if __name__ == "__main__":
+    print(f"Sequential time: {draw_sequential_test(g, eqs)}")
+    cv2.imshow("Graph", g.img)
+    cv2.waitKey(0)
+    g.reset()
 
 
-print(f"Multithreding time: {draw_multithreading_test(g, eqs)}")
-cv2.imshow("Graph", g.img)
-cv2.waitKey(0)
-g.reset()
+    print(f"Multithreding time: {draw_multithreading_test(g, eqs)}")
+    cv2.imshow("Graph", g.img)
+    cv2.waitKey(0)
+    g.reset()
 
-print(f"Multiprocessing time: {draw_multiprocessing_test(g, eqs)}")
-cv2.imshow("Graph", g.img)
-cv2.waitKey(0)
-g.reset()
+
+    print(f"Multiprocessing time: {draw_multiprocessing_test(g, eqs)}")
+    cv2.imshow("Graph", g.img)
+    cv2.waitKey(0)
+    g.reset()
