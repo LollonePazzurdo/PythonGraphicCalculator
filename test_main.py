80c20bfff4f682b3de8c5a1211120704388894df
LollonePazzurdo <lorenzogiovannini774@gmail.com>
2026-07-17 12:03:36 +0200

    refactor(test_main): Simplify multithreading test by using lambda and remove unused function

--- test_main.py
+++ test_main.py
@@ -3,9 +3,7 @@ import threading
 from graphiccalculator import *
 
 
-def draw_thread_test(g: Graph, eq:str):
-    g.draw(eq, color=colors.r_color())
-    
+ 
     
 def draw_sequential_test(g: Graph, eqs:list[str]):
     t0 = time.perf_counter()
@@ -14,14 +12,24 @@ def draw_sequential_test(g: Graph, eqs:list[str]):
     return time.perf_counter()-t0
 
 def draw_multithreading_test(g: Graph, eqs:list[str]):
+
     t0 = time.perf_counter()
     threads = []
     for eq in eqs:
-        t = threading.Thread(target=draw_thread_test, args=(g, eq))
+        t = threading.Thread(target=lambda g, eq: g.draw(eq, color=colors.r_color()), args=(g, eq))
         t.start()
         threads.append(t)
     for t in threads:
         t.join()
+    return time.perf_counter()-t0
+
+
+def draw_multiprocessing_test(g: Graph, eqs:list[str]):
+    t0 = time.perf_counter()
+    plots = []
+    for eq in eqs:
+        plots.append(Plot(equation=eq, color=colors.r_color(), size=g.size, zoom=g.zoom, domain="True", center=g.center))
+    g.draw_plots(plots)
     return time.perf_counter()-t0
 
 
@@ -46,6 +54,15 @@ eqs = [
 print(f"Sequential time: {draw_sequential_test(g, eqs)}")
 cv2.imshow("Graph", g.img)
 cv2.waitKey(0)
-print(f"Parallel time: {draw_multithreading_test(g, eqs)}")
+g.reset()
+
+
+print(f"Multithreding time: {draw_multithreading_test(g, eqs)}")
 cv2.imshow("Graph", g.img)
 cv2.waitKey(0)
+g.reset()
+
+print(f"Multiprocessing time: {draw_multiprocessing_test(g, eqs)}")
+cv2.imshow("Graph", g.img)
+cv2.waitKey(0)
+g.reset()
