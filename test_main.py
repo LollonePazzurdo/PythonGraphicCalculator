b3dc389e738b6bb5b10f57c5a06af8bb5094bad5
LollonePazzurdo <lorenzogiovannini774@gmail.com>
2026-07-17 13:36:28 +0200

    refactor(test_main): Simplify multithreading test by using get_plots and draw_plots_mt

--- test_main.py
+++ test_main.py
@@ -13,16 +13,9 @@ def draw_sequential_test(g: Graph, eqs:list[str]):
     return time.perf_counter()-t0
 
 def draw_multithreading_test(g: Graph, eqs:list[str]):
-
     t0 = time.perf_counter()
-    threads = []
-    for eq in eqs:
-        # race condition is definetly a possibility here.
-        t = threading.Thread(target=g.draw, args=(eq,), kwargs={'color': colors.r_color()})
-        t.start()
-        threads.append(t)
-    for t in threads:
-        t.join()
+    plots = g.get_plots(equations=eqs, colors=[colors.r_color() for _ in range(len(eqs))])
+    g.draw_plots_mt(plots)
     return time.perf_counter()-t0
 
 
@@ -33,7 +26,7 @@ def draw_multiprocessing_test(g: Graph, eqs:list[str]):
     return time.perf_counter()-t0
 
 
-scale = 2
+scale = 1
 g = Graph(
     view=2, 
     size=400//scale,
